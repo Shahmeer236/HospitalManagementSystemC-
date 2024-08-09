cc213969f65b888d5bc1b53bea50020e257e06e6
shahmeer <shahmeer.rizwan.236@gmail.com>
2024-08-09 18:58:46 +0500

    UpdateStorageController

---
+++ HospitalManagementSystem/Services/Interface/IFileStorageService.cs
@@ -0,0 +1,6 @@
+﻿public interface IFileStorageService
+{
+    Task<string> SaveFileAsync(byte[] fileBytes, string fileName);
+    Task<byte[]> DownloadFileAsync(string filePath);
+    void DeleteFile(string filePath);
+}
